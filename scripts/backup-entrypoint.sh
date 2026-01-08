b7074c6aa29254615ae871ece061b27f4b2c8c7c
李恩超 <470360046@qq.com>
2026-01-08 09:16:19 +0800

    修改数据库连接脚本

--- scripts/backup-entrypoint.sh
+++ scripts/backup-entrypoint.sh
@@ -13,7 +13,7 @@ counter=0
 max_attempts=12
 
 while [ $counter -lt $max_attempts ]; do
-    if mysql -h erp_db -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1" 2>/dev/null; then
+    if mysql -h erp_db -u root -p"ERPSYSTE" -e "SELECT 1" 2>/dev/null; then
         echo "数据库连接成功！"
         break
     fi
